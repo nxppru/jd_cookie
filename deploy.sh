261a0e746469585779d840057750a05f472712ee
JDHelloWorld <77717629+JDHelloWorld@users.noreply.github.com>
2021-07-27 23:54:24 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# deploy.sh
+apt update -y;apt install wget xz-utils vim curl git -y;
+echo -e "set fileencodings=utf-8,ucs-bom,gb18030,gbk,gb2312,cp936\nset termencoding=utf-8\nset encoding=utf-8" > ~/.vimrc
+curl -fsSL https://deb.nodesource.com/setup_16.x > setup_16.x; bash setup_16.x; apt-get install -y nodejs
+npm config set prefix /usr/local
+npm i -g cnpm
+cnpm config set prefix /usr/local
+cnpm i -g typescript ts-node @types/node tslib pm2
+
+cd ~
+wget http://soft.vpser.net/lnmp/lnmp1.8.tar.gz -cO lnmp1.8.tar.gz && tar zxf lnmp1.8.tar.gz && cd lnmp1.8 && LNMP_Auto="y" ./install.sh nginx
+curl https://ghproxy.com/https://raw.githubusercontent.com/JDHelloWorld/jd_cookie/main/nginx.conf > /usr/local/nginx/conf/nginx.conf
+/etc/init.d/nginx restart
+
+cd /home/wwwroot/default
+git clone https://github.com/JDHelloWorld/jd_cookie
+
+cd server
+cnpm i
+pm2 start bin/www
